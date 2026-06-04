c74f5addd77e20c6a5e34d0d76b641bce45282cb
gauthier <gauthierboyenval@outlook.fr>
2026-06-04 15:48:45 +0200

    Reapply "Gérer le mode Challenge"
    This reverts commit d4c6b928ccaee766aa853fe6929cb0540ce7914c.

--- Puissance4.Interface/FenetreVictoire.xaml.cs
+++ Puissance4.Interface/FenetreVictoire.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Puissance4.Systeme;
 
 namespace Puissance4.Interface
 {
@@ -17,9 +18,11 @@ namespace Puissance4.Interface
     /// </summary>
     public partial class FenetreVictoire : Window
     {
-        public FenetreVictoire()
+        public FenetreVictoire(Partie partie, Challenge challenge)
         {
             InitializeComponent();
+            Partie Partie = partie;
+            Challenge Challenge = challenge;
         }
     }
 }
